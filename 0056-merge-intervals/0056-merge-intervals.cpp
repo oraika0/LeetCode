d45e68752b3c174b7602eeb095f960e1ebc3d52f
oraika0 <71816399+oraika0@users.noreply.github.com>
2025-07-15 19:00:52 +0800

    0056-merge-intervals 07-15-2025 N/A (0%) N/A (0%)

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // stop using vector.erase() in a loop, it cause huge overhead
         if (intervals.size() == 1) return intervals;
 
         sort(intervals.begin(),intervals.end(),[](vector<int> i, vector<int> j){return i[0] < j[0];});
-        
+        vector<vector<int>> ret;
+        ret.push_back(intervals[0]);
         for (int i = 1 ; i < intervals.size();i++){
-                if (intervals[i][0] <= intervals[i-1][1]){
-                    // overlap occur
-                    if (intervals[i][1] >= intervals[i-1][1]){
-                        // partially  overlapped
-                        intervals[i-1][1] = intervals[i][1];
-                    }
-                    intervals.erase(intervals.begin()+i);
-                    i--;
+            if (intervals[i][0] <= ret[ret.size()-1][1]){
+                // overlap occur
+                if (intervals[i][1] >= intervals[i-1][1]){
+                    // partially  overlapped
+                    ret[ret.size()-1][1] = intervals[i][1];
                 }
+            }
+            else{
+                ret.push_back(intervals[i]);
+            }
         }
-        return intervals;
+        return ret;
     }
 };
