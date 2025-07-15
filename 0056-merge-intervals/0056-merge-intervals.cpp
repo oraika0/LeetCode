58c19530f2db118533ece824562e477e4052c8e0
oraika0 <71816399+oraika0@users.noreply.github.com>
2025-07-15 18:39:45 +0800

    0056-merge-intervals 07-15-2025 451 ms (5.25%) 52.5 MB (5.02%)

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,18 +5,16 @@ public:
 
         sort(intervals.begin(),intervals.end(),[](vector<int> i, vector<int> j){return i[0] < j[0];});
         
-        for (int i = 0 ; i < intervals.size();i++){
-            for (int j = 0; j < i;j++){
-                if (intervals[i][0] <= intervals[j][1]){
+        for (int i = 1 ; i < intervals.size();i++){
+                if (intervals[i][0] <= intervals[i-1][1]){
                     // overlap occur
-                    if (intervals[i][1] >= intervals[j][1]){
+                    if (intervals[i][1] >= intervals[i-1][1]){
                         // partially  overlapped
-                        intervals[j][1] = intervals[i][1];
+                        intervals[i-1][1] = intervals[i][1];
                     }
                     intervals.erase(intervals.begin()+i);
                     i--;
                 }
-            }
         }
         return intervals;
     }
