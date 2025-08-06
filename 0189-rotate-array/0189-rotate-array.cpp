0399d6a72688441f1805bb4ae9b44f8d42918d05
oraika0 <71816399+oraika0@users.noreply.github.com>
2025-08-06 15:10:43 +0800

    0189-rotate-array 08-06-2025 0 ms (100%) 29.7 MB (37.27%)

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        if (k > nums.size()){
-            k %= nums.size();
-        }
-        vector<int> temp(nums.begin(),nums.begin()+nums.size()-k);
-        for(int i=0; i<k;i++){
-            cout << i << " " << nums.size()-k+i << endl;
-            nums[i] = nums[nums.size()-k+i];
-        }
-        for(int j=0;j<temp.size();j++){
-            nums[j+k]=temp[j];
-        }
+        k %= nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         if (k > nums.size()){
+//             k %= nums.size();
+//         }
+//         vector<int> temp(nums.begin(),nums.begin()+nums.size()-k);
+//         for(int i=0; i<k;i++){
+//             cout << i << " " << nums.size()-k+i << endl;
+//             nums[i] = nums[nums.size()-k+i];
+//         }
+//         for(int j=0;j<temp.size();j++){
+//             nums[j+k]=temp[j];
+//         }
+//     }
+// };
 
 /*
 Input: nums = [1,2,3,4,5,6,7], k = 3
 Output: [5,6,7,1,2,3,4]
-
-
-
 */
