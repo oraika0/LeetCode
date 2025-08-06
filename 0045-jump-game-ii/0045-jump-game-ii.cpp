b68a81c0571c8bdb75bc024f2efa360d43f82354
oraika0 <71816399+oraika0@users.noreply.github.com>
2025-08-06 16:56:21 +0800

    0045-jump-game-ii 08-06-2025 462 ms (5.04%) 21.3 MB (27.8%)

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,18 +2,35 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        vector<int> DP(nums.size(),INT_MAX);
-        DP[0] = 0;
+        vector<int> DP;
+        DP.push_back(0);
         for (int i = 1; i < nums.size(); i++){
+            int temp = INT_MAX;
             for (int j = 0; j < i; j++){
                 if (i <= j + nums[j]){
-                    DP[i] = min(DP[i], DP[j] + 1);
+                    temp = min(temp, DP[j] + 1);
                 }
             }
+            DP.push_back(temp);
         }
         return DP[nums.size()-1];
     }
 };
+// class Solution {
+// public:
+//     int jump(vector<int>& nums) {
+//         vector<int> DP(nums.size(),INT_MAX);
+//         DP[0] = 0;
+//         for (int i = 1; i < nums.size(); i++){
+//             for (int j = 0; j < i; j++){
+//                 if (i <= j + nums[j]){
+//                     DP[i] = min(DP[i], DP[j] + 1);
+//                 }
+//             }
+//         }
+//         return DP[nums.size()-1];
+//     }
+// };
 
 // DP way
 // DP[i] = min (DP[j] + 1) , i > j
