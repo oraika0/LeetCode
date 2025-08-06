ef0354aeee84815a600c35a52ac6393e7a32ff8e
oraika0 <71816399+oraika0@users.noreply.github.com>
2025-08-06 16:12:53 +0800

    0055-jump-game 08-06-2025 0 ms (100%) 52.2 MB (64.39%)

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        vector<bool> table(nums.size(),false);
-        table[0] = true;
-        for (int i = 0; i < nums.size()-1;i++){
-            int min;
-            if (nums[i] <= nums.size()-i){
-                min = nums[i];
-            }
-            else{
-                min = nums.size()-i;
-            }
-            for (int j = 0; j < min; j++){
-                table[i+j+1] = true;
-            }
-
-            if (table[i] == false){
+        int reach = 0;
+        for (int i = 0; i < nums.size();i++){
+            if (reach < i){
                 return false;
             }
+            reach = std::max(nums[i]+i,reach);
         }
-        if (table[nums.size()-1] == true){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return true;
     }
 };
+// time comp:O(n)
+//space comp:O(1)
+
+// class Solution {
+// public:
+//     bool canJump(vector<int>& nums) {
+//         vector<bool> table(nums.size(),false);
+//         table[0] = true;
+//         for (int i = 0; i < nums.size()-1;i++){
+//             int min;
+//             if (nums[i] <= nums.size()-i){
+//                 min = nums[i];
+//             }
+//             else{
+//                 min = nums.size()-i;
+//             }
+//             for (int j = 0; j < min; j++){
+//                 table[i+j+1] = true;
+//             }
+
+//             if (table[i] == false){
+//                 return false;
+//             }
+//         }
+//         if (table[nums.size()-1] == true){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+// };
+// time : O(n^2)
+// space : O(n)
