cb50ea776abc05313253c76d797c528aee4703d8
oraika0 <71816399+oraika0@users.noreply.github.com>
2025-08-11 13:59:57 +0800

    0036-valid-sudoku 08-11-2025 8 ms (27.79%) 23 MB (65.31%)

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -7,14 +7,17 @@ public:
         for (int i = 0; i < 9;i++){
             for (int j = 0; j < 9; j++){
             int squarePos = (i/3)*3 + j/3; // floor i to 3's multiple
+            int val = board[i][j]-'1';
                 if (board[i][j] != '.'){
-                    if(row[i][board[i][j]-'1'] == true || column[j][board[i][j]-'1'] == true || square[squarePos][board[i][j]-'1'] == true){
+                    if(row[i][val] == true ||
+                        column[j][val] == true ||
+                        square[squarePos][val] == true){
                         return false;
                     } 
                     else{
-                        row[i][board[i][j]-'1'] = true;
-                        column[j][board[i][j]-'1'] = true;
-                        square[squarePos][board[i][j]-'1'] = true;
+                        row[i][val] = true;
+                        column[j][val] = true;
+                        square[squarePos][val] = true;
                     }
                 } 
             }
