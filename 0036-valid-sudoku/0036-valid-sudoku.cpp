f42ec17dcf633a8dbbaab165da8d6904fd8ac575
oraika0 <71816399+oraika0@users.noreply.github.com>
2025-08-11 13:59:02 +0800

    0036-valid-sudoku 08-11-2025 5 ms (44.53%) 23.1 MB (63.12%)

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,60 +1,84 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<bool> table(9,false);
-        
-        // row checking
-        for(auto row : board){
-            fill (table.begin(),table.end(),false);
-            for (auto element : row){
-                if (element != '.'){
-                    if(table[element-'1'] == false){
-                        table[element-'1'] = true;
-                    } 
-                    else{
-                        return false;
-                    }
-                }
-            }
-        }
-
-        // column checking
+        vector<vector<bool>> row(9,vector<bool>(9,false));
+        vector<vector<bool>> column(9,vector<bool>(9,false));
+        vector<vector<bool>> square(9,vector<bool>(9,false));
         for (int i = 0; i < 9;i++){
-            fill (table.begin(),table.end(),false);
             for (int j = 0; j < 9; j++){
-                if (board[j][i] != '.'){
-                    if(table[board[j][i]-'1'] == false){
-                        table[board[j][i]-'1'] = true;
+            int squarePos = (i/3)*3 + j/3; // floor i to 3's multiple
+                if (board[i][j] != '.'){
+                    if(row[i][board[i][j]-'1'] == true || column[j][board[i][j]-'1'] == true || square[squarePos][board[i][j]-'1'] == true){
+                        return false;
                     } 
                     else{
-                        return false;
+                        row[i][board[i][j]-'1'] = true;
+                        column[j][board[i][j]-'1'] = true;
+                        square[squarePos][board[i][j]-'1'] = true;
                     }
                 } 
             }
         }
-
-        // square checking
-        for (int i = 0;i < 9;i+=3){
-            for (int j = 0; j < 9; j+=3){
-                cout << endl;
-                fill (table.begin(),table.end(),false);
-                for (int in_i = 0; in_i < 3; in_i++){
-                    for (int in_j = 0; in_j < 3; in_j++){
-                        if (board[i+in_i][j+in_j] != '.'){
-                            if(table[board[i+in_i][j+in_j]-'1'] == false){
-                                table[board[i+in_i][j+in_j]-'1'] = true;
-                            } 
-                            else{
-                                return false;
-                            }
-                        }       
-                    }    
-                }
-            }
-        }
-
-
-
         return true;
     }
 };
+// class Solution {
+// public:
+//     bool isValidSudoku(vector<vector<char>>& board) {
+//         vector<bool> table(9,false);
+        
+//         // row checking
+//         for(auto row : board){
+//             fill (table.begin(),table.end(),false);
+//             for (auto element : row){
+//                 if (element != '.'){
+//                     if(table[element-'1'] == false){
+//                         table[element-'1'] = true;
+//                     } 
+//                     else{
+//                         return false;
+//                     }
+//                 }
+//             }
+//         }
+
+//         // column checking
+//         for (int i = 0; i < 9;i++){
+//             fill (table.begin(),table.end(),false);
+//             for (int j = 0; j < 9; j++){
+//                 if (board[j][i] != '.'){
+//                     if(table[board[j][i]-'1'] == false){
+//                         table[board[j][i]-'1'] = true;
+//                     } 
+//                     else{
+//                         return false;
+//                     }
+//                 } 
+//             }
+//         }
+
+//         // square checking
+//         for (int i = 0;i < 9;i+=3){
+//             for (int j = 0; j < 9; j+=3){
+//                 cout << endl;
+//                 fill (table.begin(),table.end(),false);
+//                 for (int in_i = 0; in_i < 3; in_i++){
+//                     for (int in_j = 0; in_j < 3; in_j++){
+//                         if (board[i+in_i][j+in_j] != '.'){
+//                             if(table[board[i+in_i][j+in_j]-'1'] == false){
+//                                 table[board[i+in_i][j+in_j]-'1'] = true;
+//                             } 
+//                             else{
+//                                 return false;
+//                             }
+//                         }       
+//                     }    
+//                 }
+//             }
+//         }
+
+
+
+//         return true;
+//     }
+// };
